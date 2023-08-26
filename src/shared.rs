fb46944937909c67e42e6fa80d1545904a86f693
Nordine Bittich <nordine@bittich.be>
2023-08-26 11:31:32 +0200

    autocommit

--- src/shared.rs
+++ src/shared.rs
@@ -3,6 +3,7 @@ pub const XSD_BOOLEAN: &str = "http://www.w3.org/2001/XMLSchema#boolean";
 pub const XSD_INTEGER: &str = "http://www.w3.org/2001/XMLSchema#integer";
 pub const XSD_DECIMAL: &str = "http://www.w3.org/2001/XMLSchema#decimal";
 pub const XSD_DOUBLE: &str = "http://www.w3.org/2001/XMLSchema#double";
+#[allow(unused)]
 pub const LANG_LITERAL: &str = "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString";
 pub const NS_TYPE: &str = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 pub const RDF_NIL: &str = "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil";
